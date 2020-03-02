6666d2302a7520765af85d093d14c43fabd1565b
agulbs <agulbs>
2020-03-02 16:04:03 -0500

    new templates

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 API_BASE = 'https://accounts.spotify.com'
-CLI_ID = "35e93c5e89ed499e99a335d0bc07c5de"
-CLI_SEC = "20e32dbb41cc4d289aacd9d1782f2abb"
+CLI_ID = ""
+CLI_SEC = ""
 REDIRECT_URI = "http://127.0.0.1:5000/callback"
 SCOPE = "user-read-private,user-read-email,user-read-playback-state,user-modify-playback-state"
 SHOW_DIALOG = True # False if in prod
